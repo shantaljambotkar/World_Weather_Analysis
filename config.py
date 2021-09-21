71d2fb307aa40ad09f4eabe7b8aa9fbe1ee6d392
shantaljambotkar <88519257+shantaljambotkar@users.noreply.github.com>
2021-09-20 21:32:15 -0400

    update api keys

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
 weather_api_key='d82315617a31ae0da2844e056aa6eee6'
 # Google API Key
 g_key = "AIzaSyDGAnj4hIVTgq6Nh0kPjNBFtwP_OytTmNY"
-api_key = 'qzwfkfHilGGUGqVk6hapnZHYHwDL16s2'
+
+# new york times key
+ny_api_key = 'qzwfkfHilGGUGqVk6hapnZHYHwDL16s2'
